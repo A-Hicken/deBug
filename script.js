c59590b2642c8f506d7403c2b5cb34c1f2053c38
A-Hicken <amberlie.hicken@gmail.com>
2023-11-17 12:49:14 -0700

    final commit

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // make sure these match the HTML element names for the ID then class.
-let input = document.querySelector("#NAME");
-let display = document.querySelector(".displaymycode");
+let input = document.querySelector("#name");
+let display = document.querySelector(".display");
 
 // This code should be working
-input.addEventListener("input", function () {
-//   const name = input.value;
-//   display.textContent = name;
-// });
-
+input.addEventListener("input", () => {
+  document.getElementById("name").innerHTML = "input";
+  let name = input.value;
+  display.textContent = name;
+});
 // missing function?
 
 // you should see 5 in the output of your console
-console.log(simpleMath(2, 3));
+const a = 2;
+const b = 3;
+
+console.log(a + b);
